4197a2a18345c74a52a155476523ccb7ba0b21ce
Dennis Weber <dennis@dennis-weber.info>
2020-01-30 21:59:57 +0100

    Switch to very simple Ratpack application

--- src/main/java/info/dennis_weber/unfima/api/Application.java
+++ src/main/java/info/dennis_weber/unfima/api/Application.java
@@ -1,37 +1,41 @@
 package info.dennis_weber.unfima.api;
 
-import info.dennis_weber.unfima.api.resources.v1_0.RootResource;
 import org.flywaydb.core.Flyway;
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
+import ratpack.server.RatpackServer;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 class Application {
-    private URI baseUri;
-    private HttpServer server;
     private Logger logger;
+    private Flyway flyway;
 
     // database config
     private String databaseJdbcUrl;
     private String databaseUsername;
     private String databasePassword;
-    private boolean databaseCleanOnStartup;
 
-    public static void main(String[] args) {
-        // Set base URI
-        URI baseUri;
-        try {
-            baseUri = new URI("http://localhost:8080");
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
+    /**
+     * Creates a new instance of the Unfima application which can then be started by calling {@link #startServer()}.
+     *
+     * @param databaseJdbcUrl        database connection url. e.g. "jdbc:mariadb://localhost:3306/unfima"
+     * @param databaseUsername       database username
+     * @param databasePassword       database password
+     */
+    Application(String databaseJdbcUrl, String databaseUsername, String databasePassword) {
+        this.databaseJdbcUrl = databaseJdbcUrl;
+        this.databaseUsername = databaseUsername;
+        this.databasePassword = databasePassword;
 
+        logger = Logger.getLogger(Application.class.getName());
+        flyway = Flyway.configure().dataSource(databaseJdbcUrl, databaseUsername, databasePassword).load();
+    }
+
+    /**
+     * Main method, called when starting the program
+     *
+     * @param args arguments for the program
+     */
+    public static void main(String[] args) throws Exception {
         // Read db connection config from env variables
         String databaseJdbcUrl = System.getenv("UNFIMA_DATABASE_JDBC_URL");
         if (databaseJdbcUrl == null) {
@@ -50,56 +54,32 @@ class Application {
             cleanOnStartup = true;
         }
 
-        Application mainApp = new Application(baseUri, databaseJdbcUrl, databaseUsername, databasePassword, cleanOnStartup);
+        Application mainApp = new Application(databaseJdbcUrl, databaseUsername, databasePassword);
+
+        if (cleanOnStartup) {
+            mainApp.cleanupDatabase();
+        }
+
         mainApp.startServer();
-
-        System.out.println("Application started. Stop the application using CTRL+C");
-        try {
-            Thread.currentThread().join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
-    /**
-     * Creates a new instance of the Unfima application which can then be started by calling {@link #startServer()}.
-     *
-     * @param baseUri                application url, including port to use. e.g. "http://localhost:8080"
-     * @param databaseJdbcUrl        database connection url. e.g. "jdbc:mariadb://localhost:3306/unfima"
-     * @param databaseUsername       database username
-     * @param databasePassword       database password
-     * @param databaseCleanOnStartup set to true if you want to remove all data from the database on startup (useful for tests)
-     */
-    Application(URI baseUri, String databaseJdbcUrl, String databaseUsername, String databasePassword, boolean databaseCleanOnStartup) {
-        this.baseUri = baseUri;
-        this.databaseJdbcUrl = databaseJdbcUrl;
-        this.databaseUsername = databaseUsername;
-        this.databasePassword = databasePassword;
-        this.databaseCleanOnStartup = databaseCleanOnStartup;
-
-        logger = Logger.getLogger(Application.class.getName());
+    public void cleanupDatabase() {
+        flyway.clean();
+        // TODO: Throw exception when called while app is already running
+        // TODO: Javadoc
     }
 
-    public void startServer() {
+    public void startServer() throws Exception {
         // Setup database
-        Flyway flyway = Flyway.configure().dataSource(databaseJdbcUrl, databaseUsername, databasePassword).load();
-        if (databaseCleanOnStartup) {
-            flyway.clean();
-        }
         flyway.migrate();
 
-        // Start Grizzly HTTP Server
-        try {
-            final ResourceConfig resourceConfig = new ResourceConfig(RootResource.class);
-            server = GrizzlyHttpServerFactory.createHttpServer(baseUri, resourceConfig, false);
-            Runtime.getRuntime().addShutdownHook(new Thread(server::shutdownNow));
-            server.start();
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, null, e);
-        }
-    }
+        // Start Ratpack Server
+        RatpackServer.start(server -> server
+                .handlers(chain -> chain
+                        .get(ctx -> ctx.render("Hello World!"))
+                        .get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!"))
+                )
+        );
 
-    public void stopServer() {
-        server.shutdownNow();
     }
 }
--- src/test/groovy/info/dennis_weber/unfima/api/UnfimaSpecification.groovy
+++ src/test/groovy/info/dennis_weber/unfima/api/UnfimaSpecification.groovy
@@ -2,38 +2,16 @@ package info.dennis_weber.unfima.api
 
 import spock.lang.Specification
 
-import javax.ws.rs.core.UriBuilder
-
 abstract class UnfimaSpecification extends Specification {
     Application application
-
-    private String TEST_URL_BASE = "http://localhost:8080"
-    private List<HttpURLConnection> connections = []
 
     def setup() {
         final String databaseJdbcUrl = "jdbc:h2:~/unfimaInMemoryTestDb;MODE=MySQL;DATABASE_TO_LOWER=TRUE"
         final String databaseUsername = "sa"
         final String databasePassword = null
 
-        application = new Application(new URI(TEST_URL_BASE), databaseJdbcUrl, databaseUsername, databasePassword, true)
+        application = new Application(databaseJdbcUrl, databaseUsername, databasePassword)
+        application.cleanupDatabase()
         application.startServer()
     }
-
-    def cleanup() {
-        connections.each {
-            it.disconnect()
-        }
-
-        application.stopServer()
-    }
-
-    HttpURLConnection getConnection(String path) {
-        URL url = UriBuilder.fromUri(TEST_URL_BASE + path).build().toURL()
-        HttpURLConnection connection = url.openConnection() as HttpURLConnection
-
-        connections.add(connection)
-
-        return connection
-    }
-
 }
