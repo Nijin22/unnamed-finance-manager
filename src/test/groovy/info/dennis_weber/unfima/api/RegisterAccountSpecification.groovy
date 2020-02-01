32b93194bf921d2e7406db3de8d011214b569550
Dennis Weber <dennis@dennis-weber.info>
2020-02-01 16:32:37 +0100

    Add tests for registration with invalid parameters

--- src/test/groovy/info/dennis_weber/unfima/api/RegisterAccountSpecification.groovy
+++ src/test/groovy/info/dennis_weber/unfima/api/RegisterAccountSpecification.groovy
@@ -47,7 +47,87 @@ class RegisterAccountSpecification extends AbstractUnfimaSpecification {
     resp.body.text.contains("email address is already in use")
   }
 
-  // TODO: Tests with bad request syntax
+  def "Registering without email"() {
+    given:
+    String password = "doesNotMatter"
 
+    when:
+    String json = JsonOutput.toJson(["password": password])
+    client.requestSpec({
+      it.body({
+        it.type("application/json")
+        it.text(json)
+      })
+    })
+    ReceivedResponse resp = client.post("v1.0/users")
+
+    then:
+    resp.statusCode == 400
+    resp.body.text.contains("required parameter 'email' is missing")
+  }
+
+  def "Registering with too long email"() {
+    given:
+    String email = "a.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way" +
+        ".way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way" +
+        ".way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way.way" +
+        ".way.way.way.way.way.way.way.way.way.way.way.way@to.long.email.address"
+    String password = "doesNotMatter"
+
+    when:
+    String json = JsonOutput.toJson(["email": email, "password": password])
+    client.requestSpec({
+      it.body({
+        it.type("application/json")
+        it.text(json)
+      })
+    })
+    ReceivedResponse resp = client.post("v1.0/users")
+
+    then:
+    resp.statusCode == 400
+    resp.body.text.contains("'email' parameter is too long")
+  }
+
+  def "Registering without password"() {
+    given:
+    String email = "a.new.user@to.add"
+
+    when:
+    String json = JsonOutput.toJson(["email" : email])
+    client.requestSpec({
+      it.body({
+        it.type("application/json")
+        it.text(json)
+      })
+    })
+    ReceivedResponse resp = client.post("v1.0/users")
+
+    then:
+    resp.statusCode == 400
+    resp.body.text.contains("required parameter 'password' is missing")
+  }
+
+  def "registering with a too long password"() {
+    given:
+    String email = "a.new.user@to.add"
+    String password = "this is way, way, way, way, way, way, way, way, way, way, way, way, way, way, way, way, way," +
+        " way, way, way, way, way, way, way, way, way, way, way, way, way, way, way, way, way, way," +
+        " too long, even for a reasonable secure password, as it would allow DOS by taking too much CPU time to bcrypt!"
+
+    when:
+    String json = JsonOutput.toJson(["email": email, "password": password])
+    client.requestSpec({
+      it.body({
+        it.type("application/json")
+        it.text(json)
+      })
+    })
+    ReceivedResponse resp = client.post("v1.0/users")
+
+    then:
+    resp.statusCode == 400
+    resp.body.text.contains("'password' parameter is too long")
+  }
 
 }
