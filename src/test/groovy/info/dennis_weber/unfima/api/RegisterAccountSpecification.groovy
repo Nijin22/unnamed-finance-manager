2002ce842845f78b7775d956f99dc8f9eea30bf6
Dennis Weber <dennis@dennis-weber.info>
2020-02-01 15:23:26 +0100

    Add test for registering an email that already exists

--- src/test/groovy/info/dennis_weber/unfima/api/RegisterAccountSpecification.groovy
+++ src/test/groovy/info/dennis_weber/unfima/api/RegisterAccountSpecification.groovy
@@ -2,6 +2,7 @@ package info.dennis_weber.unfima.api
 
 import groovy.json.JsonOutput
 import info.dennis_weber.unfima.api.helpers.AbstractUnfimaSpecification
+import info.dennis_weber.unfima.api.helpers.UnfimaServerBackedApplicationUnderTest
 import ratpack.func.Action
 import ratpack.http.client.ReceivedResponse
 import ratpack.http.client.RequestSpec
@@ -32,7 +33,32 @@ class RegisterAccountSpecification extends AbstractUnfimaSpecification {
     resp.statusCode == 201
   }
 
+  def "Registering an email that already exists"() {
+    given:
+    String email = UnfimaServerBackedApplicationUnderTest.TEST_DATA.user.email
+    String password = "doesNotMatter"
+
+    when:
+    String json = JsonOutput.toJson(["email": email, "password": password])
+    client.requestSpec(new Action<RequestSpec>() {
+      @Override
+      void execute(RequestSpec requestSpec) throws Exception {
+        requestSpec.body(new Action<RequestSpec.Body>() {
+          @Override
+          void execute(RequestSpec.Body body) throws Exception {
+            body.type("application/json")
+            body.text(json)
+          }
+        })
+      }
+    })
+    ReceivedResponse resp = client.post("v1.0/users")
+
+    then:
+    resp.statusCode == 409
+  }
+
   // TODO: Tests with bad request syntax
 
-  // TODO: Test with already existing user
+
 }
--- src/test/groovy/info/dennis_weber/unfima/api/helpers/UnfimaServerBackedApplicationUnderTest.groovy
+++ src/test/groovy/info/dennis_weber/unfima/api/helpers/UnfimaServerBackedApplicationUnderTest.groovy
@@ -1,27 +1,51 @@
 package info.dennis_weber.unfima.api.helpers
 
+import groovy.sql.Sql
 import info.dennis_weber.unfima.api.Application
-import org.h2.tools.Server
 import ratpack.server.RatpackServer
 import ratpack.test.ServerBackedApplicationUnderTest
 
 import static ratpack.server.internal.ServerCapturer.capture
 
 class UnfimaServerBackedApplicationUnderTest extends ServerBackedApplicationUnderTest {
+  // Config for in-memory h2 database
+  final String databaseJdbcUrl = "jdbc:h2:mem:unfimaTestDb;MODE=MySQL;DATABASE_TO_LOWER=TRUE;DB_CLOSE_DELAY=-1"
+  final String databaseUsername = "sa"
+  final String databasePassword = null
+
+  static final Map<String, Object> TEST_DATA = [
+      "user": [
+          "email"   : "john.doe@example.org",
+          "password": "test-password"
+      ]
+  ]
+
   @Override
   protected RatpackServer createServer() throws Exception {
     // Just as MainClassApplicationUnderTest.createServer() does, we also use the "ServerCapturer" to capture
     // the RatpackServer we need to return.
     return capture({
-      // Config for in-memory h2 database
-      final String databaseJdbcUrl = "jdbc:h2:mem:unfimaTestDb;MODE=MySQL;DATABASE_TO_LOWER=TRUE;DB_CLOSE_DELAY=-1"
-      final String databaseUsername = "sa"
-      final String databasePassword = null
 
       // Setup the application with the test db
       Application application = new Application(databaseJdbcUrl, databaseUsername, databasePassword)
+
+      // Ensure a clean new database is used so all tests are independent of each other
       application.cleanupDatabase()
+
+      // Start the application
       application.startServer()
+
+      // Fill the application with test data
+      fillWithTestData()
     })
+  }
+
+  private void fillWithTestData() {
+    Sql sql = Sql.newInstance(databaseJdbcUrl, databaseUsername, databasePassword)
+
+    // Test users
+    sql.executeInsert(
+        "INSERT INTO users (email, password) VALUES(?, ?);",
+        [TEST_DATA.user.email, TEST_DATA.user.password])
   }
 }
