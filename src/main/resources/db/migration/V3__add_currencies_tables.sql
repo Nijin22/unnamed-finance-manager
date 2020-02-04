db8e5a2e71dd70d9c5b8cf606ca5b7f30c3d9470
Dennis Weber <dennis@dennis-weber.info>
2020-02-04 19:27:03 +0100

    Use currencyId instead of currencyCode
    As users should be able to track code-less "currencies" as well (like stocks)

--- src/main/resources/db/migration/V3__add_currencies_tables.sql
+++ src/main/resources/db/migration/V3__add_currencies_tables.sql
@@ -1,12 +1,11 @@
 CREATE TABLE `currencies`(
+    `currencyId` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `userId` INT NOT NULL,
-    `currencyCode` VARCHAR(3) NOT NULL,
+    `shortName` VARCHAR(255) NOT NULL,
     `fullName` VARCHAR(255) NOT NULL,
-    `symbol` VARCHAR(255) NOT NULL,
     `fractionalName` VARCHAR(255) NOT NULL,
     `decimalPlaces` INT NOT NULL,
 
-    PRIMARY KEY (`userId`, `currencyCode`),
     FOREIGN KEY (userId) REFERENCES users(id)
       ON DELETE CASCADE
       ON UPDATE CASCADE
@@ -14,17 +13,12 @@ CREATE TABLE `currencies`(
 
 CREATE TABLE `currencyExchangeRate`(
     `exchangeRateId` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `userId` INT NOT NULL,
-    `currencyCode` VARCHAR(3) NOT NULL,
+    `currencyId` INT NOT NULL,
     `startTimestamp` BIGINT NOT NULL,
     `exchangeRate` DECIMAL(65,15) NOT NULL,
     `comment` TEXT DEFAULT NULL,
 
-    FOREIGN KEY (`userId`) REFERENCES users(id)
-      ON DELETE CASCADE
-      ON UPDATE CASCADE,
-
-    FOREIGN KEY (`userId`, `currencyCode`) REFERENCES currencies(`userId`, `currencyCode`)
+    FOREIGN KEY (`currencyId`) REFERENCES currencies(`currencyId`)
       ON DELETE CASCADE
       ON UPDATE CASCADE,
 
