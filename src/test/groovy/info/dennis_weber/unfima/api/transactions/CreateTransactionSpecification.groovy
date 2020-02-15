30b8f408e532865a4d162cd11b5df886b2e6113f
Dennis Weber <dennis@dennis-weber.info>
2020-02-15 18:41:08 +0100

    Add create-transaction endpoint

---
+++ src/test/groovy/info/dennis_weber/unfima/api/transactions/CreateTransactionSpecification.groovy
@@ -0,0 +1,169 @@
+package info.dennis_weber.unfima.api.transactions
+
+import groovy.json.JsonOutput
+import info.dennis_weber.unfima.api.helpers.AbstractUnfimaSpecification
+import info.dennis_weber.unfima.api.helpers.UnfimaServerBackedApplicationUnderTest
+import info.dennis_weber.unfima.api.services.TimestampHelper
+
+class CreateTransactionSpecification extends AbstractUnfimaSpecification {
+  def "Creating a new transaction"() {
+    given:
+    Map request = ["transactionName": "Buying example groceries",
+                   "timestamp"      : TimestampHelper.currentTimestamp,
+                   "notes"          : "",
+                   balanceChanges   : [
+                       [
+                           "accountId": UnfimaServerBackedApplicationUnderTest.TEST_DATA.accounts.ownAccount.id,
+                           "value"    : new BigDecimal("-10")
+                       ],
+                       [
+                           "accountId": UnfimaServerBackedApplicationUnderTest.TEST_DATA.accounts.thirdPartyAccount.id,
+                           "value"    : new BigDecimal("10")
+                       ]
+                   ]
+    ]
+
+    when:
+    // TODO: Refactor setting the request body into a static method
+    authenticatedClient.requestSpec({
+      it.body({
+        it.type("application/json")
+        it.text(JsonOutput.toJson(request))
+      })
+    })
+    authenticatedClient.post("/v1.0/transactions")
+    def answer = getResponseObject(authenticatedClient)
+
+    then:
+    authenticatedClient.response.statusCode == 201
+    answer.class == Integer
+  }
+
+  def "Creating a new transaction but missing a required attribute"() {
+    given:
+    Map request = ["transactionName": "Buying example groceries",
+                   "timestamp"      : TimestampHelper.currentTimestamp,
+                   "notes"          : "",
+                   // Note that the balance changes are missing
+    ]
+
+    when:
+    authenticatedClient.requestSpec({
+      it.body({
+        it.type("application/json")
+        it.text(JsonOutput.toJson(request))
+      })
+    })
+    authenticatedClient.post("/v1.0/transactions")
+    def answer = getResponseObject(authenticatedClient)
+
+    then:
+    authenticatedClient.response.statusCode == 400
+    answer.errorMsg == "Required parameter 'balanceChanges' is missing."
+    answer.errorId == "REQUIRE_PARAMETER_MISSING"
+  }
+
+  def "Creating a new transaction with unbalanced 'balance changes'"() {
+    given:
+    Map request = ["transactionName": "Buying example groceries",
+                   "timestamp"      : TimestampHelper.currentTimestamp,
+                   "notes"          : "",
+                   balanceChanges   : [
+                       [
+                           "accountId": UnfimaServerBackedApplicationUnderTest.TEST_DATA.accounts.ownAccount.id,
+                           "value"    : new BigDecimal("-10")
+                       ],
+                       [
+                           "accountId": UnfimaServerBackedApplicationUnderTest.TEST_DATA.accounts.thirdPartyAccount.id,
+                           "value"    : new BigDecimal("10.01")
+                       ]
+                   ]
+    ]
+
+    when:
+    authenticatedClient.requestSpec({
+      it.body({
+        it.type("application/json")
+        it.text(JsonOutput.toJson(request))
+      })
+    })
+    authenticatedClient.post("/v1.0/transactions")
+    def answer = getResponseObject(authenticatedClient)
+
+    then:
+    authenticatedClient.response.statusCode == 400
+    answer.errorMsg.startsWith("All involved currencies in a transaction need to equal to 0.")
+    answer.errorId == "UNBALANCED_BALANCE_CHANGES"
+  }
+
+  def "Creating a new transaction with three affected accounts"() {
+    given:
+    Map request = ["transactionName": "Buying example groceries",
+                   "timestamp"      : TimestampHelper.currentTimestamp,
+                   "notes"          : "",
+                   balanceChanges   : [
+                       [
+                           "accountId": UnfimaServerBackedApplicationUnderTest.TEST_DATA.accounts.ownAccount.id,
+                           "value"    : new BigDecimal("-10")
+                       ],
+                       [
+                           "accountId": UnfimaServerBackedApplicationUnderTest.TEST_DATA.accounts.anotherOwnAccount.id,
+                           "value"    : new BigDecimal("-5.12")
+                       ],
+                       [
+                           "accountId": UnfimaServerBackedApplicationUnderTest.TEST_DATA.accounts.thirdPartyAccount.id,
+                           "value"    : new BigDecimal("15.12")
+                       ]
+                   ]
+    ]
+
+    when:
+    authenticatedClient.requestSpec({
+      it.body({
+        it.type("application/json")
+        it.text(JsonOutput.toJson(request))
+      })
+    })
+    authenticatedClient.post("/v1.0/transactions")
+    def answer = getResponseObject(authenticatedClient)
+
+    then:
+    authenticatedClient.response.statusCode == 201
+    answer.class == Integer
+  }
+
+  def "Creating a new transaction with an invalid account id"() {
+    given:
+    int invalidAccountId = 987
+    Map request = ["transactionName": "Buying example groceries",
+                   "timestamp"      : TimestampHelper.currentTimestamp,
+                   "notes"          : "",
+                   balanceChanges   : [
+                       [
+                           "accountId": invalidAccountId,
+                           "value"    : new BigDecimal("-10")
+                       ],
+                       [
+                           "accountId": UnfimaServerBackedApplicationUnderTest.TEST_DATA.accounts.thirdPartyAccount.id,
+                           "value"    : new BigDecimal("10")
+                       ]
+                   ]
+    ]
+
+    when:
+    authenticatedClient.requestSpec({
+      it.body({
+        it.type("application/json")
+        it.text(JsonOutput.toJson(request))
+      })
+    })
+    authenticatedClient.post("/v1.0/transactions")
+    def answer = getResponseObject(authenticatedClient)
+
+    then:
+    authenticatedClient.response.statusCode == 400
+    answer.errorMsg == "Cannot create a balance change for account '$invalidAccountId' as this account does not exist"
+    answer.errorId == "ACCOUNT_NOT_VALID"
+  }
+
+}
